36510e1f106afffcb4b3b75afde95f4b4ff41ac1
jeankass1980 <jeankass1980@gmail.com>
2018-03-03 17:28:29 +0100

    tryout

---
+++ accueil.php
@@ -0,0 +1,104 @@
+<!DOCTYPE html>
+<html lang="fr-FR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="description" content="ophtalmologiste chirurgie réfractive Lemans">
+	<meta name="author" content="NolifeRapha">  
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/styleBscreen.css">
+    <title>Chirurgie réfractive Dr AGOSSOU</title> 
+</head>
+<body>
+    <header>     
+        <div class="logo">
+            <p class = "logo"> <img src="images/imglogo" alt="logo du cabinet"> </p>
+            <p class="phrase1">Chirurgie réfractive par laser</p>
+            <p class="phrase2">Docteur AGOSSOU Komlan</p>
+        </div>
+        <div class="container-info1">
+            <div class="container-info2">
+                <div class="adress">
+                    <p class = ""> <img src="images/logoadr.png" alt="adresse du cabinet"> 21 Rue de La Martinière (ZA) 72300 Sablé-sur-Sarthe </p>
+                </div>
+                <div class="telephone">
+                    <p > <img src="images/logotel.png" alt="téléphone cabinet"> 02 43 95 87 66 </p>
+                </div>
+            </div>
+             <div id="nav">
+                <ul>
+                    <li><a href="#">Accueil</a></li>
+                    <li><a href="#">Suis-je opérable?</a></li>
+                    <li><a href="#">Les traitements</a></li>
+                    <li><a href="#">Prendre rendez-vous</a></li>
+                </ul>
+             </div>
+        </div>
+    </header>
+    <section class="container-rdv">
+        <div class="presentation-img">
+            <img src="images/smile.webp" alt="image de présentation">
+           
+        </div>
+        <div class="timetable">
+            <div class="horaire"></div>
+            <div class="takerdv"></div>
+        </div>
+    </section>
+
+    <section class="description">
+        <div class="chir">
+            <p>Situé au coeur de la clinique de l&rsquo;anjou, le centre de chirurgie réfractive &laquo;&nbsp;Anjou vision&nbsp;&raquo;, traite vos défauts visuels au laser. Les ophtalmologues vous accompagnent durant les étapes clés de votre opération&#8230;</p>
+
+            <p><iframe style="width: 100%;" src="https://www.youtube.com/embed/QHENzRSlWw8?rel=0" width="300" height="315" frameborder="0" allowfullscreen="allowfullscreen"></iframe></p>
+
+        </div>
+        <div class="pourquoi">
+                <h3>Motivation des patients</h3>
+                <div>
+                    <p>Les motivations des patients pour la chirurgie réfractive sont diverses et correspondent à des enjeux pratiques, fonctionnels, esthétiques et financiers. L’estime de soi est également partie prenante chez certains patients, en particuliers atteints de fortes amétropies (myopie forte, etc.). Les facteurs principaux mis en avant par les patients (souvent associés) pour justifier la réalisation d’une chirurgie réfractive sont :</p>
+                    <ul>
+                        <li>– l’intolérance aux lentilles de contact</li>
+                        <li>– l’aspect inesthétique des verres de lunettes</li>
+                        <li>– le sentiment d’insécurité voir de handicap provoqué par le dépendance à une correction optique (forts amétropes)</li>
+                        <li>– le besoin de pratiquer certaines activités sportives ou de loisir sans lunettes ou lentilles (natation, voile, ski, moto, voyages et séjour en milieu naturel ou hostile, etc.)</li>
+                        <li>– l’amélioration de la vision</li>
+                    </ul>
+                </div>
+
+                <div>
+                    <div>
+                        <div ><span>1</span></div>
+                        <h4> Diagnostic pré opératoire </h4>
+                        <p>Ces examens permettront de vérifier que vous ne présentez aucune contre indication au traitement laser.</p>
+                    </div>
+                    
+                    <div>
+                        <div ><span >2</span></div>
+                        <h4> Traitement </h4>
+                        <p>Le traitement au laser dure environt 30 minutes pour les 2 yeux.<br />
+                          Myopie / Hypermétropie / Astigmatisme / Presbytie</p>
+                    </div>
+
+                    <div >
+                        <div ><span >3</span></div>
+                        <h4>Visite post opératoire</h4>
+                        <p>Vous réalisez une visite post opératoire chez votre ophtalmologiste dans les jours qui suivent le traitement.</p>
+                    </div>
+
+                </div>
+    </section>
+    <section class="foot1">
+        <div class="coord">
+        </div>
+        <div class="pourqui">
+        </div>
+        <div class="traitement">
+        </div>
+        <div class="takerdv2">
+        </div>
+    </section>
+    <footer id ="footer">
+    </footer>
+</body>
+</html>
